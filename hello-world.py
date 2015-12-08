8f9e57c1842deb0b9272764c8e3f1f4356da8fa7
root <root@beaglebone.localdomain>
2015-12-08 03:50:18 +0000

    First Commit

---
+++ hello-world.py
@@ -0,0 +1,12 @@
+# This a test of anything and everything for the UPAS calibration rig
+# It will test the:
+#	Beaglebone printing and SSH interface (Done)
+#	The LCD screen
+#	The BME280
+# 	Uploading files and directories to github
+
+
+
+#If you're reading this on github then github is working!
+
+
